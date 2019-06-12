23683ae1aa7ab13c92c9074fc60488325dc919bb
LucyMHall <lucymhall1995@gmail.com>
2019-06-12 17:35:15 +0100

    Working on getting the form to work

--- app.rb
+++ app.rb
@@ -3,29 +3,14 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
 get '/' do
-'Testing infrastructure working!'
   erb (:index)
 end
 
 post '/'do
-
   @name1 = params['Player_1']
   @name2 = params['Player_2']
-  'Player 1: #{@name1} V Player 2: #{@name2}'
+  "Player 1: #{@name1} V Player 2: #{@name2}"
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   run! if app_file == $0
