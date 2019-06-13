966b0c517fb70e2b099c0951b526f217184418ef
LucyMHall <lucymhall1995@gmail.com>
2019-06-13 11:01:03 +0100

    Add Player 2 hp to /play

--- spec/feature/testing_infrastructure_spec.rb
+++ spec/feature/testing_infrastructure_spec.rb
@@ -1,7 +1,6 @@
 require './app.rb'
 
 describe "Homepage", type: :feature do
-
   it 'gets Player1 and Player2\'s names and displays them'  do
     visit('/names')
     fill_in 'Player_1', with: "Lucy"
@@ -9,6 +8,14 @@ describe "Homepage", type: :feature do
     click_on 'Submit'
     expect(page).to have_content 'Lucy Vs Carl'
   end
+end
 
-
+describe "/names", type: :feature do
+  it 'displays Player2\'s hit points' do
+    visit('/names')
+    fill_in 'Player_1', with: "Lucy"
+    fill_in 'Player_2', with: "Carl"
+    click_on 'Submit'
+    expect(page).to have_content 'Carl Hit Points: 60'
+  end
 end
