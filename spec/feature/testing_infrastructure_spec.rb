578a220446a5df716412474d34a01faff2b0f1e8
LucyMHall <lucymhall1995@gmail.com>
2019-06-13 17:26:33 +0100

    Tidying up testing_infrastructure_spec.rb

--- spec/feature/testing_infrastructure_spec.rb
+++ spec/feature/testing_infrastructure_spec.rb
@@ -1,24 +1,24 @@
 require './app.rb'
 require_relative 'web_helpers.rb'
 
-describe "/play", type: :feature do
-  it 'gets Player1 and Player2\'s names and displays them'  do
+feature 'viewing the homepage' do
+  scenario 'displays Player1 and Player2 names' do
     sign_in_and_play
     expect(page).to have_content 'Lucy Vs Carl'
   end
-  it 'displays Player2\'s hit points' do
+  scenario 'displays Player2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Carl Hit Points: 60'
   end
-  it 'allows Player 1 to attack Player 2 and get a confirmation' do
+  scenario 'allows Player 1 to attack Player 2 and get a confirmation' do
     sign_in_and_play
     click_on 'attack'
     expect(page).to have_content 'Lucy attacked Carl'
   end
 end
 
-describe "/attacked", type: :feature do
-  it 'reduces Player 2 hp by 10' do
+feature 'viewing the attack page' do
+  scenario 'when you click attack it reduces Player 2 HP by 10' do
     sign_in_and_play
     click_on 'attack'
     expect(page).to have_content 'Carl Hit Points: 50'
