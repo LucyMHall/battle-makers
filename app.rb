4cee9eec48124a9b752aabaf4d773db6f0d0d6c0
LucyMHall <lucymhall1995@gmail.com>
2019-06-13 11:27:32 +0100

    Player 1 can attack Player 2 and have it confirmed

--- app.rb
+++ app.rb
@@ -1,24 +1,33 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-enable :sessions
+  enable :sessions
 
-get '/names' do
-  erb (:index)
-end
+  get '/names' do
+    erb (:index)
+  end
 
-post '/names' do
-  session['player1_name'] = params['Player_1']
-  session['player2_name'] = params['Player_2']
-  redirect "/play"
-end
+  post '/names' do
+    session['player1_name'] = params['Player_1']
+    session['player2_name'] = params['Player_2']
+    redirect "/play"
+  end
 
-get '/play' do
-  @name_1 = session['player1_name']
-  @name_2 = session['player2_name']
-  erb (:play)
-end
+  get '/play' do
+    @name_1 = session['player1_name']
+    @name_2 = session['player2_name']
+    erb (:play)
+  end
 
+  post '/play' do
+    redirect "/attack"
+  end
+
+  get '/attack' do
+    @name_1 = session['player1_name']
+    @name_2 = session['player2_name']
+    erb (:attack)
+  end
 
   run! if app_file == $0
 end
--- spec/feature/testing_infrastructure_spec.rb
+++ spec/feature/testing_infrastructure_spec.rb
@@ -1,16 +1,18 @@
 require './app.rb'
 require_relative 'web_helpers.rb'
 
-describe "Homepage", type: :feature do
+describe "/play", type: :feature do
   it 'gets Player1 and Player2\'s names and displays them'  do
     sign_in_and_play
     expect(page).to have_content 'Lucy Vs Carl'
   end
-end
-
-describe "/names", type: :feature do
   it 'displays Player2\'s hit points' do
     sign_in_and_play
     expect(page).to have_content 'Carl Hit Points: 60'
+  end
+  it 'allows Player 1 to attack Player 2 and get a confirmation' do
+    sign_in_and_play
+    click_on 'attack'
+    expect(page).to have_content 'Lucy attacked Carl'
   end
 end
